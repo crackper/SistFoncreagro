2e725b01a960c0724ab2dcff0de9fe7de556b414
crackper <samuel.mestanza@hotmail.com>
2014-03-18 10:57:18 -0500

    Agreando famework CSS y alistando prototipo para proyectos....

--- SistFoncreagro.WebSite/Monitoreo/Formularios/FormDatosProyecto.aspx
+++ SistFoncreagro.WebSite/Monitoreo/Formularios/FormDatosProyecto.aspx
@@ -3,15 +3,81 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
     </telerik:RadAjaxManager>
-    <telerik:RadAutoCompleteBox ID="radcbPersonal" runat="server" Culture="es-ES" 
-        DataSourceID="odsPersonal" AutoPostBack="True" EmptyMessage="Ingrese apellidos o nombres"
-        DataTextField="nombreCompleto" 
-        DataValueField="IdPersonal" 
-        DropDownWidth="400px"
-        Width="400px" Skin="Hay" TextSettings-SelectionMode="Single">
-<TextSettings SelectionMode="Single"></TextSettings>
-    </telerik:RadAutoCompleteBox>
-    <asp:ObjectDataSource ID="odsPersonal" runat="server" 
-        SelectMethod="GetAllFromPersonal" 
-        TypeName="SistFoncreagro.BussinesLogic.PersonalBL"></asp:ObjectDataSource>
+    
+    <div class="row">
+    <div class="span4">
+        <fieldset>
+            <legend>Información General del Proyecto</legend>
+            <asp:HiddenField ID="hfIdProyecto" runat="server" />
+            <label> Area: </label>
+            <asp:TextBox ID="txtArea" Enabled="False" runat="server" Width="270px"/>
+            <label>Nombre Proyecto:</label>
+            <asp:TextBox ID="txtNombreProyecto" runat="server" TextMode="MultiLine" Width="270px"/>
+            <label>Alias Proyecto:</label>
+            <asp:TextBox ID="txtAliasProyecto" runat="server" Width="270px"/>
+             <label>Responsable: </label>
+             <telerik:RadAutoCompleteBox ID="radcbPersonal" runat="server" Culture="es-ES" 
+                        DataSourceID="odsPersonal" AutoPostBack="True" 
+                        EmptyMessage="Ingrese apellidos o nombres"
+                        DataTextField="nombreCompleto" 
+                        DataValueField="IdPersonal"  
+                        DropDownWidth="270px"
+                        Width="270px"
+                        TextSettings-SelectionMode="Single" InputType="Text">
+                    <TextSettings SelectionMode="Single"></TextSettings>
+                    </telerik:RadAutoCompleteBox>
+                    <asp:ObjectDataSource ID="odsPersonal" runat="server" 
+                        SelectMethod="GetAllFromPersonal" 
+                        TypeName="SistFoncreagro.BussinesLogic.PersonalBL"/>
+             <label>Alineamiento:</label>
+             <asp:DropDownList ID="cbAlineamiento" runat="server" Width="270px">
+                <asp:ListItem Value="0">[- Seleccione uno -]</asp:ListItem>
+                <asp:ListItem>SALUD</asp:ListItem>
+                <asp:ListItem>EDUCACION</asp:ListItem>
+                <asp:ListItem>GESTION DEL AGUA</asp:ListItem>
+                <asp:ListItem>DESARROLLO ECONOMICO</asp:ListItem>
+             </asp:DropDownList>
+             <label>Línea de Intervención:</label>
+             <asp:DropDownList ID="cbPrograma" runat="server" Width="270px">
+                <asp:ListItem Value="0">[- Seleccione uno -]</asp:ListItem>
+                <asp:ListItem>DESARROLLO GANADERO</asp:ListItem>
+                <asp:ListItem>SEGURIDAD ALIMENTARIA</asp:ListItem>
+                <asp:ListItem>SANEAMIENTO BASICO</asp:ListItem>
+             </asp:DropDownList>
+             <label>Inicio Programado:</label>
+            <telerik:RadDatePicker ID="rdpInicioProgramado" Runat="server" FocusedDate="1985-01-01"/>
+             <label>Fin Programado:</label>
+            <telerik:RadDatePicker ID="rdpFinProgramado" Runat="server" FocusedDate="1985-01-01"/>
+            <label>Estado:</label>
+            <asp:DropDownList ID="cbEstado" runat="server" Width="270px">
+                <asp:ListItem>IMPLEMENTACION</asp:ListItem>
+                <asp:ListItem>EJECUCION</asp:ListItem>
+                <asp:ListItem>CIERRE</asp:ListItem>
+            </asp:DropDownList>              
+        </fieldset>
+    </div>
+    <div class="span8">
+        <telerik:RadTabStrip ID="rtsEstructura" runat="server" 
+            MultiPageID="rmpEstructura" SelectedIndex="0">
+            <Tabs>
+                <telerik:RadTab runat="server" ImageUrl="../../img/category16x16.png" Text="Estructura" Selected="True"/>
+                <telerik:RadTab runat="server" ImageUrl="../../img/clip16x16.png" Text="Adjuntos"/>
+            </Tabs>
+        </telerik:RadTabStrip>
+        <telerik:RadMultiPage ID="rmpEstructura" runat="server" SelectedIndex="0">
+            <telerik:RadPageView ID="RadPageView1" runat="server" alt="Estructura">
+                <fieldset>
+                    <legend>Convenios</legend>
+                </fieldset>
+            </telerik:RadPageView>
+            <telerik:RadPageView ID="RadPageView2" runat="server" alt="Adjuntos">
+                <fieldset>
+                    <legend>Documentos Adjuntos</legend>
+                </fieldset>
+            </telerik:RadPageView>
+        </telerik:RadMultiPage>
+    </div>
+ </div>
+
+       
 </asp:Content>
--- SistFoncreagro.WebSite/Monitoreo/Formularios/FormDatosProyecto.aspx.designer.vb
+++ SistFoncreagro.WebSite/Monitoreo/Formularios/FormDatosProyecto.aspx.designer.vb
@@ -23,6 +23,42 @@ Partial Public Class FormDatosProyecto
     Protected WithEvents RadAjaxManager1 As Global.Telerik.Web.UI.RadAjaxManager
 
     '''<summary>
+    '''Control hfIdProyecto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents hfIdProyecto As Global.System.Web.UI.WebControls.HiddenField
+
+    '''<summary>
+    '''Control txtArea.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtArea As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtNombreProyecto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtNombreProyecto As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control txtAliasProyecto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtAliasProyecto As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
     '''Control radcbPersonal.
     '''</summary>
     '''<remarks>
@@ -39,4 +75,85 @@ Partial Public Class FormDatosProyecto
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents odsPersonal As Global.System.Web.UI.WebControls.ObjectDataSource
+
+    '''<summary>
+    '''Control cbAlineamiento.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cbAlineamiento As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Control cbPrograma.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cbPrograma As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Control rdpInicioProgramado.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents rdpInicioProgramado As Global.Telerik.Web.UI.RadDatePicker
+
+    '''<summary>
+    '''Control rdpFinProgramado.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents rdpFinProgramado As Global.Telerik.Web.UI.RadDatePicker
+
+    '''<summary>
+    '''Control cbEstado.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cbEstado As Global.System.Web.UI.WebControls.DropDownList
+
+    '''<summary>
+    '''Control rtsEstructura.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents rtsEstructura As Global.Telerik.Web.UI.RadTabStrip
+
+    '''<summary>
+    '''Control rmpEstructura.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents rmpEstructura As Global.Telerik.Web.UI.RadMultiPage
+
+    '''<summary>
+    '''Control RadPageView1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents RadPageView1 As Global.Telerik.Web.UI.RadPageView
+
+    '''<summary>
+    '''Control RadPageView2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents RadPageView2 As Global.Telerik.Web.UI.RadPageView
 End Class
